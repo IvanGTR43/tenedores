bf76f9dabab473faf8380ecdf1a85695ae39dcbb
IvanGTR43 <ivan_tr43@outlook.com>
2021-04-14 21:25:54 -0500

    hasta aqui

--- app/components/Restaurants/ListRestaurants.js
+++ app/components/Restaurants/ListRestaurants.js
@@ -25,13 +25,31 @@ export default function ListRestaurant(props){
 
 function Restaurant(props){
     const {restaurant} = props
-
+    console.log(restaurant);
+    const {images, name, description, adrress} = restaurant.item
+    imgeRestaurant = images[0]
+    const goRestaurant =() =>{
+        console.log("Hola Touvhabek");
+    }
     return(
-        <View>
-            <Text>
-                Restaurante
-            </Text>
-        </View>
+        <TouchableOpacity
+            onPress={goRestaurant}>
+                <View style={styles.viewRestaurant}>
+                    <View style={styles.viewRestaurantImage}>
+                        <Image
+                            resizeMode="cover"
+                            PlaceholderContent={<ActivityIndicator color="#fff"/>}
+                            source={imgeRestaurant ? {uri: imgeRestaurant} : require("../../../assets/img/no-image.png")}
+                            style={styles.imageRestaurant}
+                        />
+                    </View>
+                    <View>
+                        <Text style={styles.restaurantName}>{name}</Text>
+                        <Text style={styles.restaurantAddress}>{adrress}</Text>
+                        <Text style={styles.restaurantDescription}>{description.substr(0, 60)}...</Text>
+                    </View>
+                </View>
+        </TouchableOpacity>
     )
 }
 
@@ -40,5 +58,28 @@ const styles = StyleSheet.create({
         marginTop: 10,
         marginBottom: 10,
         alignItems: "center"
+    },
+    viewRestaurant:{
+        flexDirection: "row",
+        margin: 10
+    },
+    viewRestaurantImage:{
+        marginRight: 15
+    },
+    imageRestaurant: {
+        width: 80,
+        height:80
+    },
+    restaurantName:{
+        fontWeight: "bold"
+    },
+    restaurantAddress:{
+        paddingTop: 2,
+        color: "grey"
+    },
+    restaurantDescription:{
+        paddingTop: 2,
+        color: "grey",
+        width: 300
     }
 })
